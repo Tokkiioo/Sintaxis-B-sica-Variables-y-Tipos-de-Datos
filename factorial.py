0fceb0eb181b32c98500c83092a9403be644833f
Carlos Serrano <128412240+Tokkiioo@users.noreply.github.com>
2024-12-10 15:05:49 -0300

    Ejercicio factorial agregado

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# Ejercicio calculo de un factorial de un número
+
+# Usando la función factorial del modulo math
+
+import math
+
+factorial = int(input("Ingrese el número del factorial que quiere encontrar: "))
+
+print("El factorial del número es: ")
+
+print(math.factorial(factorial))
+
